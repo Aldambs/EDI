dd10db4d79dc094eccf2b60144af1cc58c601bbd
Aldambs <36906787+Aldambs@users.noreply.github.com>
2019-07-26 21:36:29 -0300

    Add files via upload

--- Position.java
+++ Position.java
@@ -1,8 +1,32 @@
+
 public class Position {
 
     private int [][][] row  = new int[][][];
     private int [][][] col = new int[][][];
 
-    
+    public int[][][] getCol() {
+        return col;
+    }
 
+    public int[][][] getRow() {
+        return row;
+    }
+
+    public void setCol(int[][][] col) {
+        this.col = col;
+    }
+
+    public void setRow(int[][][] row) {
+        this.row = row;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
