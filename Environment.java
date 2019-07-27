324590c7b1582bf25847a3777f917d620af78045
Aldambs <36906787+Aldambs@users.noreply.github.com>
2019-07-26 21:47:59 -0300

    Add files via upload

--- Environment.java
+++ Environment.java
@@ -1,2 +1,12 @@
 public class Environment {
+
+    private char[][] environment;
+
+    public Environment(char[][] environment){
+        this.environment = environment;
+    }
+
+    public Environment(int width, int length){
+        this.width = width;
+    }
 }
