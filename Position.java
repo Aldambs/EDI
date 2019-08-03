7186c79a7b34d9591f5d974fdc4f2d4f9e02f5de
Aldambs <36906787+Aldambs@users.noreply.github.com>
2019-08-02 21:36:10 -0300

    Update Position.java

--- Position.java
+++ Position.java
@@ -6,8 +6,8 @@ public class Position {
 	private int col;
 	
 	public Position(int row, int col) {
-		this.row=row;
-		this.col=col;
+		this.row = row;
+		this.col = col;
 	}
 	
 	public int getRow() {
